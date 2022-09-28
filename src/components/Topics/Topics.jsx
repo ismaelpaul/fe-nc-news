729574e8c15a1d23f8dbdccf2a00d755ac0adec4
ismaelpaul <ismaafp@gmail.com>
2022-09-28 09:19:39 +0100

    Add loading spinner

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -1,18 +1,38 @@
 import './Topics.css';
 import axios from 'axios';
+import { Oval } from 'react-loader-spinner';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Topics = () => {
 	const [topics, setTopics] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		axios
 			.get('https://news-backend-project.herokuapp.com/api/topics')
 			.then(({ data }) => {
 				setTopics(data.topic);
+				setIsLoading(false);
 			});
 	}, []);
+
+	if (isLoading) {
+		return (
+			<div className="spinner-container">
+				<Oval
+					className="spinner"
+					wrapperClass="spinner"
+					height={50}
+					width={50}
+					color="red"
+					secondaryColor="red"
+					strokeWidth={5}
+					strokeWidthSecondary={5}
+				/>
+			</div>
+		);
+	}
 
 	return (
 		<section className="topic-body">
@@ -20,7 +40,10 @@ const Topics = () => {
 				{topics.map((topic) => {
 					return (
 						<li key={topic.slug} className="topic-card">
-							<Link className="topic-links" to={`/articles/${topic.slug}`}>
+							<Link
+								className="topic-links"
+								to={`/articles/topic/${topic.slug}`}
+							>
 								<h2>{topic.description}</h2>
 								<p>{topic.slug}</p>
 							</Link>
