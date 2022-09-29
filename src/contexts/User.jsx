c0bc5fc33d604957abcafee11194c06d4a302a59
ismaelpaul <ismaafp@gmail.com>
2022-09-29 18:52:23 +0100

    Create user  logged in context

---
+++ src/contexts/User.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+export const UserProvider = (props) => {
+	const [loggedInUser, setLoggedInUser] = useState({
+		username: 'cooljmessy',
+		name: 'Peter Messy',
+		avatar_url:
+			'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002',
+	});
+
+	return (
+		<UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
+			{props.children}
+		</UserContext.Provider>
+	);
+};
