4e91a4936cf4386ed11e8d950bcfefbd2fec3bcd
ismaelpaul <ismaafp@gmail.com>
2022-09-29 08:49:43 +0100

    Add vote on article and reorder card

--- src/components/SingleArticle/SingleArticle.jsx
+++ src/components/SingleArticle/SingleArticle.jsx
@@ -1,14 +1,15 @@
 import './SingleArticle.css';
 import { Oval } from 'react-loader-spinner';
-import { FaRegCommentDots } from 'react-icons/fa';
-import { TbArrowBigTop, TbArrowBigDown } from 'react-icons/tb';
+import { BsHandThumbsUp, BsHandThumbsDown } from 'react-icons/bs';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { GoComment } from 'react-icons/go';
+import moment from 'moment';
 
 const SingleArticle = () => {
 	const { article_id } = useParams();
-	const [article, setArticle] = useState([]);
+	const [article, setArticle] = useState({});
 	const [isLoading, setIsLoading] = useState(true);
 	const [error, setError] = useState(null);
 
@@ -25,6 +26,48 @@ const SingleArticle = () => {
 				setError(err.response.data.msg);
 			});
 	}, [article_id]);
+
+	const articleVotesUp = () => {
+		setArticle((currentArticle) => {
+			return { ...currentArticle, votes: currentArticle.votes + 1 };
+		});
+		const reqBody = {
+			inc_votes: 1,
+		};
+
+		axios
+			.patch(
+				`https://news-backend-project.herokuapp.com/api/articles/${article_id}`,
+				reqBody
+			)
+			.then((data) => {})
+			.catch((err) => {
+				setArticle((currentArticle) => {
+					return { ...currentArticle, votes: currentArticle.votes - 1 };
+				});
+			});
+	};
+
+	const articleVotesDown = () => {
+		setArticle((currentArticle) => {
+			return { ...currentArticle, votes: currentArticle.votes - 1 };
+		});
+		const reqBody = {
+			inc_votes: -1,
+		};
+
+		axios
+			.patch(
+				`https://news-backend-project.herokuapp.com/api/articles/${article_id}`,
+				reqBody
+			)
+			.then((data) => {})
+			.catch((err) => {
+				setArticle((currentArticle) => {
+					return { ...currentArticle, votes: currentArticle.votes + 1 };
+				});
+			});
+	};
 
 	if (error) {
 		return <h2>{error}</h2>;
@@ -50,22 +93,28 @@ const SingleArticle = () => {
 		<main>
 			<section className="single-article-body">
 				<ul>
-					<li key={article.article_id} className="single-article">
+					<li key={article.article_id} className="single-article-card">
+						<p className="single-article-topic">{article.topic}</p>
 						<h1>{article.title}</h1>
 						<p className="author">
-							by <strong>{article.author}</strong>
+							{' '}
+							posted by <strong>{article.author}</strong> •{' '}
+							{moment(article.created_at).fromNow()}
 						</p>
 						<p>{article.body}</p>
-						<p>Topic: {article.topic}</p>
-						<p>
-							<FaRegCommentDots /> <strong>{article.comment_count}</strong>{' '}
-							comments
-						</p>
-						<p>
-							<TbArrowBigTop /> <strong>{article.votes}</strong>{' '}
-							<TbArrowBigDown />
-						</p>
-						<p>Created at: {article.created_at}</p>
+						<div className="single-article-interaction">
+							<p>
+								<BsHandThumbsUp
+									aria-label="votes for this comment"
+									onClick={articleVotesUp}
+								/>{' '}
+								<strong>{article.votes}</strong>{' '}
+								<BsHandThumbsDown onClick={articleVotesDown} />
+							</p>
+							<p>
+								<GoComment /> <strong>{article.comment_count}</strong> comments
+							</p>
+						</div>
 					</li>
 				</ul>
 			</section>
