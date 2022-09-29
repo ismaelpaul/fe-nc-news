124819ecc81d528679fbe3e728f90e88e0a79ece
ismaelpaul <ismaafp@gmail.com>
2022-09-29 11:31:41 +0100

    Add path to comment list

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Articles from './components/Articles/Articles';
 import Topics from './components/Topics/Topics';
 import SingleArticle from './components/SingleArticle/SingleArticle';
+import CommentsList from './components/CommentsList/CommentsList';
 
 function App() {
 	return (
@@ -16,6 +17,10 @@ function App() {
 				<Route path="/topics" element={<Topics />}></Route>
 				<Route path="/articles/topic/:topic" element={<Articles />}></Route>
 				<Route path="/articles/:article_id" element={<SingleArticle />}></Route>
+				<Route
+					path="/articles/:article_id/comments"
+					element={<SingleArticle />}
+				></Route>
 			</Routes>
 		</div>
 	);
