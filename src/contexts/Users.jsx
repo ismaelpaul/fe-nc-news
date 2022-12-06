ee728a4f7c85b6678079a3c4e4ea91d2e7e302c8
ismaelpaul <ismaafp@gmail.com>
2022-12-06 09:41:49 +0000

    Add Users context

---
+++ src/contexts/Users.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react';
+
+export const UsersContext = createContext();
+
+export const UserProvider = (props) => {
+	const [allUsers, setAllUsers] = useState({});
+
+	return (
+		<UsersContext.Provider value={{ allUsers, setAllUsers }}>
+			{props.children}
+		</UsersContext.Provider>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { LoggedUserProvider } from './contexts/LoggedUser';
+import { UserProvider } from './contexts/Users';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<LoggedUserProvider>
-		<React.StrictMode>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</React.StrictMode>
+		<UserProvider>
+			<React.StrictMode>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</React.StrictMode>
+		</UserProvider>
 	</LoggedUserProvider>
 );
